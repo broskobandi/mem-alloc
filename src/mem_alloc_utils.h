66bc23216507072a1bce5b3cdf93363ea32cb90c
broskobandi <broskobandi@protonmail.com>
2025-09-05 18:00:30 +0100

    fix: Fix pointer conversion bug

--- src/mem_alloc_utils.h
+++ src/mem_alloc_utils.h
@@ -3,6 +3,109 @@
 
 #include "mem_alloc.h"
 
-// only getters for the test utility?
+#include <stdalign.h> /* for alignof(max_align_t) */
+#include <stdbool.h>
+#include <sys/mman.h> /* for mmap() and munmap() */
+
+/*****************************************************************************
+ * Macro helpers
+ ****************************************************************************/
+
+/** Rounds up 'size' to the nearest multiple of alignof(max_align_t) */
+#define ROUNDUP(size)\
+	(((size) + alignof(max_align_t) - 1) & ~(alignof(max_align_t) - 1))
+
+/** Calculates data offset from the start of the pointer metadata. */
+#define DATA_OFFSET ROUNDUP(sizeof(ptr_t))
+
+/** The minimum allocation size which is the max alignment on
+ * the platform. */
+#define MIN_ALLOC_SIZE alignof(max_align_t)
+
+/** The number of bytes that can be stored in the arena. 
+ * This can be overridden by recompiling the library with 
+ * -DARENA_SIZE_CUSTOM=<custom_size> compiler flag. */
+#ifndef ARENA_SIZE_CUSTOM
+#define ARENA_SIZE 1024LU * 128
+#else
+#define ARENA_SIZE ARENA_SIZE_CUSTOM
+#endif
+
+/** The number every possible allocation sizes. */
+#define NUM_PTR_SIZE_CLASSES (ARENA_SIZE - DATA_OFFSET) / MIN_ALLOC_SIZE
+
+/** Calculates size class that corresponds with 'size'. */
+#define PTR_SIZE_CLASS(size) (size) / MIN_ALLOC_SIZE 
+
+/** Shorthand for calling mmap with the standard arguments. */
+#define MMAP(size)\
+	mmap(\
+		NULL,\
+		(size),\
+		PROT_WRITE | PROT_READ,\
+		MAP_PRIVATE | MAP_ANONYMOUS,\
+		-1,\
+		0\
+	);
+
+/*****************************************************************************
+ * Structs
+ ****************************************************************************/
+
+/** Struct for storing metadata for an allocated block of memory. */
+typedef struct ptr ptr_t;
+
+/** Struct for storing metadata for the global arena. */
+typedef struct arena arena_t;
+
+/** Ptr struct definition. */
+struct ptr {
+	/** Pointer to the user owned data. */
+	void *data;
+
+	/** The (aligned and rounded up) sum of the user requested 
+	 * memory size and the metadata size. */
+	size_t total_size;
+
+	/** The next free pointer. Only relevant if the pointer was freed. */
+	ptr_t *next_free;
+
+	/** The previous free pointer. Only relevant if the pointer was
+	 * freed. */
+	ptr_t *prev_free;
+
+	/** Boolean indicating whether the pointer is valid or not.
+	 * It is set to false when the pointer is freed. Needed
+	 * for checking if expansion can be done in place. */
+	bool is_valid;
+
+	/** Boolean indicating whether the memory was allocated in the 
+	 * arena or in thea heap using mmap().*/
+	bool is_mmap;
+};
+
+/** Arena struct definition */
+struct arena {
+	/** Main buffer. */
+	alignas(max_align_t) unsigned char buff[ARENA_SIZE];
+
+	/** An array of linked list of all size classes of free pointers. */
+	ptr_t *free_ptr_tails[NUM_PTR_SIZE_CLASSES];
+
+	/** The size of the region in the arena that is currently
+	 * occupied by either valid or invalid (freed and ready to be reused)
+	 * allocations. */
+	size_t offset;
+};
+
+/*****************************************************************************
+ * Globals
+ ****************************************************************************/
+
+/** Thread local static global instance of the arena. 
+ * Each thread gets its own copy of this eliminating the need
+ * for mutex locks. */
+_Thread_local static arena_t g_arena;
+// extern arena_t g_arena;
 
 #endif
--- test/test.c
+++ test/test.c
@@ -5,8 +5,19 @@ TEST_INIT;
 
 int main(void) {
 	{
-		void *data = mem_alloc(1024);
+		size_t size = 1024;
+		void *data = mem_alloc(size);
 		ASSERT(data);
+		ptr_t *ptr = (ptr_t*)((unsigned char*)data - DATA_OFFSET);
+		printf("%p\n", ptr);
+		ASSERT((unsigned char*)ptr == &g_arena.buff[0]);
+		ASSERT(ptr);
+		ASSERT(!ptr->is_mmap);
+		ASSERT(ptr->is_valid == true);
+		ASSERT(ptr->data == data);
+		ASSERT(!ptr->next_free);
+		ASSERT(!ptr->prev_free);
+		ASSERT(ptr->total_size == DATA_OFFSET + ROUNDUP(size));
 	}
 
 	test_print_results();
