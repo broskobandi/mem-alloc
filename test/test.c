070ffbf24a12d45fa9b434f9b17b13d8fd73acec
broskobandi <broskobandi@protonmail.com>
2025-09-07 21:55:34 +0100

    feat: Update test_use_arena()

--- test/test.c
+++ test/test.c
@@ -20,10 +20,22 @@ void test_use_arena() {
 	ASSERT(!arena.free_ptr_tails[SIZE_CLASS(total_size)]);
 	ASSERT(arena.ptrs_tail == ptr);
 	ASSERT((ptr_t*)arena.buff == ptr);
+
+	void *mem2 = use_arena(total_size, &arena);
+	ptr_t *ptr2 = PTR_META(mem2);
+	ASSERT(ptr2->prev_in_arena == ptr);
+	ASSERT(ptr->next_in_arena == ptr2);
+	ASSERT(arena.ptrs_tail == ptr2);
 }
 
 void test_add_to_free_list() {
-	// arena_t arena = {0};
+	arena_t arena = {0};
+	size_t total_size = MEM_OFFSET + ROUNDUP(sizeof(int), MIN_ALLOC_SIZE);
+	void *mem = use_arena(total_size, &arena);
+	ptr_t *ptr = PTR_META(mem);
+	add_to_free_list(ptr, &arena);
+	ptr_t *tail = arena.free_ptr_tails[SIZE_CLASS(total_size)];
+	ASSERT(tail == ptr);
 }
 
 void test_use_free_list() {
@@ -33,6 +45,7 @@ void test_use_free_list() {
 
 int main(void) {
 	test_use_arena();
+	test_add_to_free_list();
 	test_use_free_list();
 
 	test_print_results();
