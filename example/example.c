e0b8b0439896409aa0d009291186a6c42ea01b55
broskobandi <broskobandi@protonmail.com>
2025-09-09 00:23:32 +0100

    feat: Add example.c

--- example/example.c
+++ example/example.c
@@ -1,5 +1,16 @@
+/* Include the library. */
 #include <mem_alloc.h>
 
-int mainv(void) {
+int main(void) {
+
+	/* Allocate new memory. */
+	void *mem = mem_alloc(1024);
+
+	/* Reallocate existing memory. */
+	mem_realloc(mem, 2048);
+
+	/* Deallocate memory. */
+	mem_free(mem);
+
 	return 0;
 }
